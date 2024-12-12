73adf146e92146cb5f69b528c5e4a6fdaccf70ee
lfm147258369 <14929113+lfm147258369@user.noreply.gitee.com>
2024-12-12 18:43:20 +0800

    力扣，PAT练习

---
+++ 代码/Leetcode/Practice_2024_12_12/practice_1021.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <unordered_map>
+#include <stack>
+#include <string>
+using namespace std;
+
+class Solution
+{
+public:
+    string removeOuterParentheses(string s)
+    {
+
+    }
+};
+
+
+int main()
+{
+    string s;
+    cin >> s;
+    Solution sol;
+    cout << sol.removeOuterParentheses(s) << endl;
+}
---
+++ 代码/Leetcode/Practice_2024_12_12/practice_20.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <unordered_map>
+#include <stack>
+using namespace std;
+
+//class Solution 
+//{
+//public:
+//    bool isValid(string s) 
+//    {
+//        if (s.length() % 2)
+//        {
+//            return false;
+//        }
+//        stack <char> st;
+//        for (char c : s)
+//        {
+//            if (c == '(')
+//                st.push(')');
+//            else if (c == '[')
+//                st.push(']');
+//            else if (c == '{')
+//                st.push('}');
+//            else
+//            {
+//                if ((st.empty()) || (st.top() != c))
+//                {
+//                    return false;
+//                }
+//                st.pop();
+//            }
+//        }
+//        return st.empty();
+//    }
+//};
+
+//class Solution
+//{
+//    unordered_map <char, char> mp = { {'(',')'}, {'[',']'}, {'{','}'} };
+//public:
+//    bool isValid(string s)
+//    {
+//        if (s.length() % 2)
+//        {
+//            return false;
+//        }
+//        stack <char> st;
+//        for (char c : s)
+//        {
+//            if (mp.find(c) != mp.end())
+//            {
+//                st.push(mp[c]);
+//            }
+//            else
+//            {
+//                if (st.empty() || st.top() != c)
+//                {
+//                    return false;
+//                }
+//                st.pop();
+//            }
+//        }
+//        return st.empty();
+//        //return true;//err
+//    }
+//};
+
+//class Solution
+//{
+//    unordered_map<char, char> mp = { {')','('}, {']','['}, {'}','{'} };
+//public:
+//    bool isValid(string s)
+//    {
+//        if (s.length() % 2)
+//        {
+//            return false;
+//        }
+//        stack <char> st;
+//        for (char c : s)
+//        {
+//            if (mp.find(c) == mp.end())//这个键一个都没有说明是左括号
+//            {
+//                st.push(c);
+//            }
+//            else
+//            {
+//                if (st.empty() || st.top() != mp[c])//知道键就知道值
+//                {
+//                    return false;
+//                }
+//                st.pop();
+//            }
+//        }
+//        return st.empty();
+//    }
+//};
+//int main()
+//{
+//    string s;
+//    cin >> s;
+//    Solution sol;
+//    cout << sol.isValid(s) << endl;
+//}
